cbbce8583ec36d4eb141f5bf646f78039737157f
Pavlo Gusar <pgusar@yahoo.com>
2019-07-24 23:54:55 +0300

    Export ListenerOptions type

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,1 @@
-export { Emitter, EMITTER_FORCE_LAYER_TYPE, EMITTER_TOP_LAYER_TYPE, } from './KeyLayers';
+export { Emitter, ListenerOptions, EMITTER_FORCE_LAYER_TYPE, EMITTER_TOP_LAYER_TYPE, } from './KeyLayers';
